5af33858a1228873ca8efa53fc9246e9745f36ad
miv <mike.verdone@ableton.com>
2011-09-14 17:44:32 +0200

    Add xmonad.hs

---
+++ X/.xmonad/xmonad.hs
@@ -0,0 +1,22 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/miv/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , terminal = "urxvt"
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        } 
+          `additionalKeys`
+        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        ]
