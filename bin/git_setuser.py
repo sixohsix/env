1faab585f209900389d08d6822c17556b5a0117b
Mike Verdone <mike.verdone@gmail.com>
2011-10-24 14:27:44 +0200

    Add a script to set user on a per-project basis.

---
+++ bin/git_setuser.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""\
+USAGE
+  git_setuser.py <work|home>
+"""
+
+userdata = dict(
+    home="""\
+[user]
+	name = Mike Verdone
+	email = mike.verdone@gmail.com
+""",
+    work="""\
+[user]
+	name = miv
+	email = mike.verdone@ableton.com
+""",
+    )
+
+import sys
+
+if __name__=='__main__':
+    args = sys.argv[1:]
+    if not args:
+        print(__doc__)
+        ret = 1
+    else:
+        open(".git/config", "a").write(userdata[args[0]])
+        ret = 0
+    sys.exit(ret)
