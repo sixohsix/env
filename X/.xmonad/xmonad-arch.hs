13cae6eed23e0aa16cbd2e6987ee8cf68076e229
Mike Verdone <mike.verdone@gmail.com>
2011-12-27 21:09:35 +0100

    Some config files for arch.

---
+++ X/.xmonad/xmonad-arch.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Prompt
+import XMonad.Prompt.RunOrRaise
+import XMonad.Prompt.XMonad
+import System.IO
+
+main = do
+  xmproc <- spawnPipe "/home/mike/.cabal/bin/xmobar ~/.xmobarrc"
+  xmonad $ defaultConfig
+    { manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    , modMask = mod4Mask
+    , terminal = "urxvt"
+    , logHook = dynamicLogWithPP xmobarPP
+      { ppOutput = hPutStrLn xmproc
+      , ppTitle = xmobarColor "green" "" . shorten 50
+      }
+    }
+    `additionalKeysP`
+    [ ("C-<Print>", spawn "sleep 0.2; scrot -s")
+    , ("M-x r", runOrRaisePrompt defaultXPConfig)
+    , ("M-x x", xmonadPrompt defaultXPConfig)
+    ]
