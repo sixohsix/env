1bc51d482abdbdbe0367ef58b2dce5ccff431ddb
miv <mike.verdone@ableton.com>
2012-12-05 12:45:35 +0100

    Make xterm backspace work correctly.

--- stuff.bash
+++ stuff.bash
@@ -31,6 +31,7 @@ alias g=git
 alias ll='ls -al'
 alias develop='. ~/bin/develop.sh'
 alias n='nosetests -vs'
+alias dn='python abl/web/manage.py test -s'
 alias b='behave --no-capture -t-browser'
 alias ssh='TERM=xterm-color ssh'
 
