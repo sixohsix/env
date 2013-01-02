22c7d6f86a16b6fc16e1d1122d8f0d3348fe656c
miv <mike.verdone@ableton.com>
2013-01-02 11:43:17 +0100

    Clean up xmonad.hs.

--- X/.xmonad/xmonad-arch.hs
+++ X/.xmonad/xmonad-arch.hs
@@ -9,24 +9,27 @@ import XMonad.Prompt.RunOrRaise
 import XMonad.Prompt.XMonad
 import System.IO
 
+myKeys = [
+  ("C-<Print>", spawn "sleep 0.2; scrot -s"),
+  ("M-x r", runOrRaisePrompt defaultXPConfig),
+  ("M-x x", xmonadPrompt defaultXPConfig),
+  ("M-="  , spawn "amixer sset Master 1.5dB+"),
+  ("M--"  , spawn "amixer sset Master 1.5dB-")
+  ]
+
+myConfig xmProc = defaultConfig {
+  manageHook = manageDocks <+> manageHook defaultConfig,
+  layoutHook = avoidStruts  $  layoutHook defaultConfig,
+  modMask = mod4Mask,
+  terminal = "xterm",
+  logHook = dynamicLogWithPP xmobarPP {
+    ppOutput = hPutStrLn xmProc,
+    ppTitle = xmobarColor "green" "" . shorten 100
+    },
+  startupHook = UF.adjustEventInput,
+  handleEventHook = UF.focusOnMouseMove
+  } `additionalKeysP` myKeys
+
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
-  xmonad $ defaultConfig
-    { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
-    , modMask = mod4Mask
-    , terminal = "xterm"
-    , logHook = dynamicLogWithPP xmobarPP
-      { ppOutput = hPutStrLn xmproc
-      , ppTitle = xmobarColor "green" "" . shorten 100
-      }
-    , startupHook = UF.adjustEventInput
-    , handleEventHook = UF.focusOnMouseMove
-    }
-    `additionalKeysP`
-    [ ("C-<Print>", spawn "sleep 0.2; scrot -s")
-    , ("M-x r", runOrRaisePrompt defaultXPConfig)
-    , ("M-x x", xmonadPrompt defaultXPConfig)
-    , ("M-="  , spawn "amixer sset Master 1.5dB+")
-    , ("M--"  , spawn "amixer sset Master 1.5dB-")
-    ]
+  xmProc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+  xmonad $ myConfig xmProc
